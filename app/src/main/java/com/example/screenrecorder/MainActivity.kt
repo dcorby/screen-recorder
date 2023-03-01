fa3502e0defce5a836acd32199da194dba3a9c3c
Your Name <you@example.com>
2023-03-01 14:13:08 -0500

    Working on the UI

--- app/src/main/java/com/example/screenrecorder/MainActivity.kt
+++ app/src/main/java/com/example/screenrecorder/MainActivity.kt
@@ -2,15 +2,21 @@ package com.example.screenrecorder
 
 import android.app.Service
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.PixelFormat
+import android.media.Image
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.provider.Settings
+import android.util.Log
 import android.view.Gravity
 import android.view.WindowManager
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.RelativeLayout.LayoutParams
 import androidx.annotation.RequiresApi
@@ -50,25 +56,81 @@ class MainActivity : AppCompatActivity() {
         override fun onCreate() {
             super.onCreate()
 
-            val params = WindowManager.LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                100,
-                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT
-            )
-            params.gravity = Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM
-            //params.horizontalMargin = 50.0.toFloat()
-            //params.verticalMargin = 50.0.toFloat()
+            windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
 
             // https://stackoverflow.com/questions/6446221/get-context-in-a-service
-            overlay = RelativeLayout(this)
-            overlay!!.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
-            overlay!!.alpha = 0.50f
-            windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
-            windowManager!!.addView(overlay, params)
+            RelativeLayout(this).let { overlay ->
+                overlay.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
+                //overlay.alpha = 0.50f
+                overlay.setBackgroundColor(Color.parseColor("#80000000"))
+                val params = WindowManager.LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    100,
+                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT
+                )
+                params.gravity = Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM
+                windowManager?.addView(overlay, params)
 
-            val hand = Handler()
+                // Create a horizontal linear layout with three equally weighted children
+                LinearLayout(this).let { layout ->
+                    val params = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+                    layout.orientation = LinearLayout.HORIZONTAL
+                    layout.layoutParams = params
+                    layout.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+                    overlay.addView(layout)
+
+                    // Create the left panel ("open full" and "go to current" icons)
+                    LinearLayout(this).let { left ->
+                        val params = LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 1.0f)
+                        left.layoutParams = params
+                        // color for test
+                        //left.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_blue))
+                        left.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+                        layout.addView(left)
+
+                        // Add the "open full" icon
+                        ImageView(this).let { image ->
+                            val params = RelativeLayout.LayoutParams(60, 60)
+                            params.leftMargin = 30
+                            params.topMargin = 20
+                            image.layoutParams = params
+                            image.setImageResource(R.drawable.ic_baseline_open_in_full_100)
+                            //image.setBackgroundColor(Color.parseColor("#80FFFFFF"))
+                            left.addView(image)
+                        }
+
+                        //val imageCurrent
+                        ImageView(this).let { image ->
+                            val params = RelativeLayout.LayoutParams(80, 80)
+                            params.leftMargin = 50
+                            params.topMargin = 10
+                            image.background = ContextCompat.getDrawable(this, R.drawable.rounded_white)
+                            image.layoutParams = params
+                            left.addView(image)
+                        }
+                    }
+
+                    // Create the center panel (record button)
+                    RelativeLayout(this).let { center ->
+                        val params = LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 1.0f)
+                        center.layoutParams = params
+                        // color for test
+                        center.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_green))
+                        layout.addView(center)
+                    }
+
+                    // Create the right panel (X to close)
+                    RelativeLayout(this).let { right ->
+                        val params = LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 1.0f)
+                        right.layoutParams = params
+                        // color for test
+                        right.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_red))
+                        layout.addView(right)
+                    }
+                }
+            }
         }
 
         override fun onBind(intent: Intent?): IBinder? {
