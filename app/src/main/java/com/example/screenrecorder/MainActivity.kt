d8e952335ac10e3615af9dd8b2ba4f9398a74dce
Your Name <you@example.com>
2023-03-02 13:54:04 -0500

    Working on permissions and the implmenting the screen recorder

--- app/src/main/java/com/example/screenrecorder/MainActivity.kt
+++ app/src/main/java/com/example/screenrecorder/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.screenrecorder
 
+import android.app.Activity
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.PixelFormat
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.MediaRecorder
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.os.IBinder
 import android.provider.Settings
 import android.view.Gravity
@@ -14,10 +22,38 @@ import android.view.WindowManager
 import android.widget.*
 import android.widget.RelativeLayout.LayoutParams
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.screenrecorder.MediaProjectionCompanion.Companion.mediaProjectionManager
+import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+// https://android.googlesource.com/platform/development/+/master/samples/ApiDemos/src/com/example/android/apis/media/projection/MediaProjectionDemo.java
+// https://github.com/Mercandj/screen-recorder-android/blob/master/app/src/main/java/com/mercandalli/android/apps/screen_recorder/main/MainActivity.kt
+// https://stackoverflow.com/questions/32381455/android-mediaprojectionmanager-in-service
+
+class MediaProjectionCompanion {
+    companion object {
+        var mediaProjectionManager: MediaProjectionManager? = null
+    }
+}
+
+class PermissionActivity : Activity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(mediaProjectionManager!!.createScreenCaptureIntent(), 1)
+    }
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "Screen Recording Permission Denied", Toast.LENGTH_SHORT).show()
+            return
+        }
+        finish()
+    }
+}
+
+class MainActivity : Activity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +66,7 @@ class MainActivity : AppCompatActivity() {
             )
             startActivityForResult(intent, 1)
         } else {
+
             startService(Intent(this, Overlay::class.java))
         }
         finish()
@@ -43,10 +80,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     class Overlay : Service() {
+
+        // Properties related to WindowManager and the UI
         private lateinit var overlay: RelativeLayout
         private lateinit var windowManager: WindowManager
         private lateinit var record: ImageView
         private lateinit var recording: ImageView
+
+        // Properties related to the screen recording
+        private var isRecording = false
+        private lateinit var mediaProjection: MediaProjection
+        private lateinit var virtualDisplay: VirtualDisplay
+        private lateinit var mediaProjectionCallback: MediaProjectionCallback
+        private lateinit var mediaRecorder: MediaRecorder
 
         @RequiresApi(Build.VERSION_CODES.O)
         override fun onCreate() {
@@ -194,7 +240,7 @@ class MainActivity : AppCompatActivity() {
                         params.topMargin = 20
                         image.layoutParams = params
                         image.setImageResource(R.drawable.ic_baseline_close_100)
-                        image.setOnClickListener { stop() }
+                        image.setOnClickListener { exit() }
                         right.addView(image)
                     }
                     layout.addView(right)
@@ -206,20 +252,71 @@ class MainActivity : AppCompatActivity() {
             if (imageView.tag.toString() == "record") {
                 record?.visibility = LinearLayout.GONE
                 recording?.visibility = LinearLayout.VISIBLE
+                startRecording()
             }
             if (imageView.tag.toString() == "recording") {
                 recording?.visibility = LinearLayout.GONE
                 record?.visibility = LinearLayout.VISIBLE
+                stopRecording()
             }
         }
 
-        private fun stop() {
+        private fun startRecording() {
+            try {
+                mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                mediaRecorder.setOutputFile(Environment.getExternalStorageDirectory().toString() + "/video.mp4")
+                mediaRecorder.setVideoSize(500, 500)
+                mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+                mediaRecorder.setVideoEncodingBitRate(512 * 1_000)
+                mediaRecorder.setVideoFrameRate(16)
+                mediaRecorder.setVideoEncodingBitRate(3_000_000)
+                mediaRecorder.setOrientationHint(0)
+                mediaRecorder.prepare()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            mediaProjection.registerCallback(mediaProjectionCallback, null)
+            virtualDisplay = mediaProjection.createVirtualDisplay(
+                "MainActivity",
+                500,
+                500,
+                2,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.surface,
+                null,
+                null
+            )
+            mediaRecorder.start()
+            isRecording = true
+        }
+
+        private fun stopRecording() {
+            virtualDisplay.release()
+            mediaProjection.unregisterCallback(mediaProjectionCallback)
+            mediaProjection.stop()
+            mediaRecorder.stop()
+            mediaRecorder.reset()
+            isRecording = false
+        }
+
+        private inner class MediaProjectionCallback : MediaProjection.Callback() {
+            override fun onStop() {
+                stopRecording()
+            }
+        }
+
+        private fun exit() {
             cleanUp()
             stopForeground(true)
             stopSelf()
         }
 
         private fun cleanUp() {
+            stopRecording()
+
+            // Clean up the WindowManager
             if (overlay.parent == null) {
                 // Already cleaned up
                 return
@@ -245,4 +342,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
