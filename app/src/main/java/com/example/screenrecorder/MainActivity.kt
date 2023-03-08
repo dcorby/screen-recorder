bcc94670c65f174525e4bc7de7c83b98344416ae
Your Name <you@example.com>
2023-03-08 00:38:38 -0500

    Add some todos

--- app/src/main/java/com/example/screenrecorder/MainActivity.kt
+++ app/src/main/java/com/example/screenrecorder/MainActivity.kt
@@ -1,6 +1,7 @@
 TODO: 1) Allow rename (blue button in listview)
       2) when user presses play initially, just load the video and immediately pause at seek(0)
       3) allow user to seek by touching bar
+      4) Add sound??
 
 package com.example.screenrecorder
 
